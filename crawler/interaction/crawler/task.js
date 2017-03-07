20def0d43b2275924b0882ca61f032f68b48c7d4
shuyzhou <big_zhou@126.com>
2017-03-07 17:38:49 +0800

    模块化任务四

--- crawler/interaction/crawler/task.js
+++ crawler/interaction/crawler/task.js
@@ -1,65 +1,73 @@
-var page = require('webpage').create(),
-	system = require('system'),
-	option = require('./option.json'),
-	key,
-	device,
-	file,
-	result;
-key = system.args[1];
-device = system.args[2];
-if(!!device){
-	page.settings.userAgent = option[device].userAgent;
-	page.viewportSize = {
-	  width: option[device].width,
-	  height: option[device].height
-	};
-}
-page.open('http://www.baidu.com/s?wd=' + encodeURIComponent(key), function(status) {
-	if(status == "success"){
-		var t = Date.now();
-    	result = {
-			code: 1,
-			msg: '抓取成功',
-			word: key,
-			time: 0,
-			dataList: []
+const phantom = require('phantom');
+const request = require('request');
+const fs = require('fs');
+const option = require('./option.json');
+module.exports = task;
+async function task(key,device) {
+    const instance = await phantom.create();
+    const page = await instance.createPage();
+    let result;
+    if(!!device){
+		page.settings.userAgent = option[device].userAgent;
+		page.viewportSize = {
+	  		width: option[device].width,
+	  		height: option[device].height
 		};
-		try {
-    		var dataList= page.evaluate(function () {
-    			return $('.c-container').map(function() {
-                	var data = {};
+	};
+	const url = `http://www.baidu.com/s?wd=${encodeURIComponent(key)}`
+    result = await crawler(url);
+    return result;
+//    await instance.exit();
+    //private function to get result
+    async function crawler(url) {
+		const status = await page.open(url);
+		const t = Date.now();
+		let result;
+		if (status !== "success") {
+			throw Error({ message: '打开页面失败!' });
+		}
+		else {
+			try {
+    			let dataList = await page.evaluate(function (){
+    				return $('.c-container').map(function (){
+                		var data = {};
                 		data.title = $(this).find('.t > a').text() || '';
                 		data.info = $(this).find('.c-abstract').text() || '';
                 		data.link = $(this).find('.t > a:first-child').attr('href') || '';
                 		data.pic = $(this).find('img.c-img').attr('src') || '';
-                	return data;
-            	}).toArray();
-    		});
-    		result.dataList = dataList;
-    		result.time = Date.now() - t;
-    	}
-    	catch (e){
-    		result = {
-				code: 0,
-				msg: '抓取失败',
-				word: key,
-				time: 0,
-				dataList: []
-			};
-    	}
-    	console.log(JSON.stringify(result));
-    	phantom.exit();
+                		return data;
+            		}).toArray();
+    			});
+    			saveImages(dataList);
+    			result = {
+					code: 1,
+					msg: '抓取成功',
+					word: key,
+					time: Date.now() - t,
+					device:device,
+					dataList: dataList
+				};
+    		}
+    		catch (e){
+    			throw Error({ message: e });
+    		}
+		}
+		await instance.exit();
+		return result;
 	}
-	else {
-		result = {
-			code: 0,
-			msg: '抓取失败',
-			word: key,
-			time: 0,
-			device: device,
-			dataList: []
-		};
-		console.log(JSON.stringify(result));
-		phantom.exit();
+	//private function to download images
+	function saveImages(dataList) {
+		dataList.forEach(function (data){
+      		if (!!data.pic) {
+        		const picId = uid();
+        		const fileName = `public/images/${picId}.jpg`;
+        		request(data.pic).pipe(fs.createWriteStream(fileName));
+        		data.picId = `images/${picId}.jpg`;
+      		}
+  		});
 	}
-});
+	//private function
+	function uid() {
+  		return Math.random().toString(36).slice(2);
+	}
+};
