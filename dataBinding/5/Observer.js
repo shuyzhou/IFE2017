5e9013bbfca993e59d82106134bb569edf7062f3
shuyzhou <big_zhou@126.com>
2017-03-10 14:59:10 +0800

    first commit

---
+++ dataBinding/5/Observer.js
@@ -0,0 +1,73 @@
+function observerFactory(data) {
+    return {
+        data: new Observer(data),
+        $watch: function (key,callback) {
+            this.data.$watch(key,callback);
+        }
+    }
+}
+// 观察者构造函数
+function Observer(data,supervisor = null) {
+    this.watcher = new Event();
+    this.supervisor = supervisor;
+    this.watcher.listen('all',(val) => {
+        if (this.supervisor === null) {return;}
+        this.supervisor.trigger(this.$parent,val);
+        this.supervisor.trigger('all',val);
+    });
+    this.walk(data);
+}
+
+let p = Observer.prototype;
+
+p.walk = function (obj) {
+    for (let [key, val] of Object.entries(obj)) {
+        // 这里进行判断，如果还没有遍历到最底层，继续new Observer
+        if (typeof val === 'object') {
+            val = new Observer(val,this.watcher);
+        }
+        this.convert(key, val); 
+    }
+};
+//为某个属性绑定getter和setter
+p.convert = function (key, val) {
+    const self = this;
+    Object.defineProperty(this, key, {
+        enumerable: true,
+        configurable: true,
+        get: function () {
+            val.$parent = val.$parent || key;
+            return val;
+        },
+        set: function (newVal) {
+            if (newVal === val) return;
+            if (typeof newVal === 'object') {
+                val = new Observer(newVal,this.watcher);
+            }
+            val = newVal;
+            this.watcher.trigger(key,newVal);
+            this.watcher.trigger('all',newVal);
+        }
+    })
+};
+p.$watch = function (key,callback) {
+    let keyList = key.split('.');
+    let observer;
+    let prop;
+    if(keyList.length > 1) {
+        keyList.reduce((obj,key) => {
+            if (obj.hasOwnProperty(key)) {
+                    observer = obj;
+                    prop = key;
+                    return obj[key];
+            } else {
+                throw Error('The property does not exist!');
+            }
+        }, this);
+    }
+    else {
+        observer = this;
+        prop = keyList[0];
+    }
+    observer.watcher.listen(prop,callback);
+}
---
+++ dataBinding/5/index.js
@@ -0,0 +1,53 @@
+class Vue {
+    constructor(option) {
+        this.ele = document.querySelector(option.el);
+        this.data = observerFactory(option.data);
+        this.parse = eval(this.compile());
+        this.init();
+    }
+    init() {
+        this.data.$watch('all',this.render.bind(this));
+        this.render();
+    }
+    render() {
+        const html = this.parse(this.data.data);
+        if(!this.html || this.html != html){
+            this.html = this.ele.innerHTML = html;
+        }
+    }
+    compile() {
+        const evalExpr = /\{\{([\w\.]*(\[\d+\])*)\}\}/g;
+        let template = this.ele.innerHTML
+                            .replace(/[\r\t\n]/g, " ")
+                            .replace(evalExpr,"',data.$1,'");
+
+        template = "output.push('" + template + "');";
+
+        const script =
+            `(function parse(data){
+                var output = [];
+                ${ template };
+                return output.join("");
+            })`;
+
+        return script;
+    }
+}
+
+let app = new Vue({
+  el: '#app',
+  data: {
+    user: {
+      name: 'youngwind',
+      age: 25
+    },
+    school: 'bupt',
+    major: 'computer'
+  }
+});
+app.data.data.school = 'SJTU';
+app.data.data.user.name = 'xiaoming';
+app.data.data.user = {
+    name: 'sophia',
+    age: 18
+}
