753683cdc9907edbbacaf39f5b02401dd914155c
shuyzhou <big_zhou@126.com>
2017-04-16 21:44:47 +0800

    bug fix

--- crawler/concurrent/concurrent/index.js
+++ crawler/concurrent/concurrent/index.js
@@ -4,21 +4,19 @@ const dataDAO = require("../crawler/model/data");
 module.exports = async function run(socket){
 	socket.on('crawler', async function (data) {
 		var queue = async.queue(function(data, callback) {
-        let {word,page,device} = data;
-        task(socket,word,page,device);
+            let {word,page,device} = data;
+            task(socket,word,page,device);
     		callback();
 		}, 5);
 		data.deviceList.forEach(function (device) {
-  			queue.push({word: data.word,page: data.page,device: device}
-        ,function (err) {
-  			   console.log(err);
-  		  });
+  			queue.push({word: data.word,page: data.page,device: device})
+        });
 	});
 }
 
 async function task(socket,word,page,device) {
-  let result = await crawler(word,page,device);
-  await dataDAO.save(result);
-  socket.emit('result',result);
-  return result;
+    let result = await crawler(word,page,device);
+    await dataDAO.save(result);
+    socket.emit('result',result);
+    return result;
 }
