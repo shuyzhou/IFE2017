ce8130d122a26f93f10254ceddc73018c844bbbf
shuyzhou <big_zhou@126.com>
2017-11-07 14:33:23 +0800

    update code for slider

--- slider3D/README.md
+++ slider3D/README.md
@@ -1,2 +1,2 @@
 ### IFE2017
-CSS3实现3D 轮播图
+CSS3实现3D轮播图，详解请看张鑫旭的[博客](http://www.zhangxinxu.com/wordpress/2012/09/css3-3d-transform-perspective-animate-transition/)
--- slider3D/js/main.js
+++ slider3D/js/main.js
@@ -4,21 +4,10 @@ class Slider {
 		this.init();
 	}
 	init() {
-		const imgList = Array.from(this.ele.querySelectorAll('.image'));
-		this.stage = [
-			[1,2,3,4,5,6],
-			[2,3,4,5,6,1],
-			[3,4,5,6,1,2],
-			[4,5,6,1,2,3],
-			[5,6,1,2,3,4],
-			[6,1,2,3,4,5]
-		];
+		const wrapper = this.ele.querySelector('.wrapper');
 		this.count = 0;
 		document.onclick = () => {
-			this.count = this.count == 6?0:this.count;
-			imgList.forEach((img,idx) => {
-				img.style.animation = 'slider-stage'+this.stage[idx][this.count]+' 1s linear forwards';
-			});
+			wrapper.style.transform = 'rotateY('+this.count*60+'deg)';
 			this.count++;
 		}
 	}
